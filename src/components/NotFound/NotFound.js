c0bf8928ee9a938db51a296c41de195cc1e9e382
rabbymohiuddin <rabbymohiuddin@gmail.com>
2020-10-14 12:09:11 +0600

    not found route created

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="container py-5 text-danger">
+      <h3 className="text-center">404! Page you requested not found !!!</h3>
+    </div>
+  );
+};
+
+export default NotFound;
