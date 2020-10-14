47fe974683ea047047f4d36c91d4e8e5e704064e
rabbymohiuddin <rabbymohiuddin@gmail.com>
2020-10-14 12:01:41 +0600

    app.js updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { AuthProvider } from "./assets/hooks/useAuth";
+import About from "./components/About/About";
+import Blogs from "./components/Blogs/Blogs";
 import Booking from "./components/Booking/Booking";
 import Header from "./components/Header/Header";
 import Coxsbazar from "./components/Map/Coxsbazar";
 import Sreemangal from "./components/Map/Sreemangal";
 import Sundarban from "./components/Map/Sundarban";
+import News from "./components/News/News";
 import Slider from "./components/Slider/Slider";
 const App = () => {
   return (
@@ -27,6 +30,15 @@ const App = () => {
           </Route>
           <Route path="/hotel/room/Info/sreemangal">
             <Sreemangal></Sreemangal>
+          </Route>
+          <Route path="/news">
+            <News></News>
+          </Route>
+          <Route path="/blogs">
+            <Blogs></Blogs>
+          </Route>
+          <Route path="/about">
+            <About></About>
           </Route>
         </Switch>
       </AuthProvider>
